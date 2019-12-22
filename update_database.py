f2d98a7dfd31f5b75e228f1b9e501ab6d2d57302
justynamaciag <justynamaciag96@gmail.com>
2019-12-22 20:38:14 +0100

    Delete file update_database.py

--- update_database.py
+++
@@ -1,8 +0,0 @@
-def insert_tweet(connection, tweet):
-    # think if it's not better to give a list of tweets as a parameter
-    cursor = connection.cursor()
-    query = "INSERT INTO tweets(id, userId, fullText, createdAt, inReplyToTweetId, inReplyToUserId,) " \
-            "VALUES(%s, %s, %s, %s, %s, %s)"
-    data = (tweet.id, tweet.user_id, tweet.full_text, tweet.created_at, tweet.in_reply_to_tweet_id, tweet.in_reply_to_user_id)
-    cursor.execute(query, data)
-    connection.commit()
